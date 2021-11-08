d243e221570bc86fb2e09032e6d56e4c1bdfe0cf
ArnonAsquira <arnonafking2010@gmail.com>
2021-11-08 16:42:50 +0200

    a

--- back/app.js
+++ back/app.js
@@ -19,7 +19,7 @@ app.use(cors({
     methods: '*'
 }));
 
-app.use(express.static(`../dist`));
+app.use(express.static(path.resolve(__dirname,`../dist`)));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../dist/index.html"));

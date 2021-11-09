ef1c80e9613bea17f57cbfbfd9989769899eebc2
ArnonAsquira <arnonafking2010@gmail.com>
2021-11-09 12:49:58 +0200

    adding a mongodb file and a mongodb functionality to display and save phoneBook data in a cloud mongodb server

--- back/app.js
+++ back/app.js
@@ -7,25 +7,54 @@ const getRouters = require('./routers/getRouters');
 const postRouters = require('./routers/postRouters');
 const morgan = require('morgan');
 const serveStatic = require('serve-static');
+const mongoose = require('mongoose');
+const { response } = require('express');
 
+// accessing the password from the gitigore file
+let password;
+try {
+    password = fs.readFileSync(path.resolve(__dirname, '../superSecretPasswordTomongo.json'),'utf-8');
+    console.log(password);
+} catch(error) {
+    console.log('password could not be accesed');
+}
+
+
+// url adress to acces mongodb
+const url = `mongodb+srv://Arnon-Asquira:${password}@cluster0.sihgr.mongodb.net/arnonsfirstdatabase?retryWrites=true&w=majority`;
+
+// mongoose logic
+mongoose.connect(url);
+
+const phoneEntrySchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Entry = mongoose.model('Entry', phoneEntrySchema);
+
+// intilizing the app
 const app = express();
 
-app.use(express.json())
+app.use(express.json()) // parses request body to json
 
 const port = process.env.PORT || 3001;
 
+// disableling cors
 app.use(cors({
     origin: '*',
     methods: '*'
 }));
 
+// serving the javascript file for the index html staticly so that it can be used
 app.use(express.static(path.resolve(__dirname,`../dist`)));
 
+// serving the index html file to the client for interaction with the rest of the api
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../dist/index.html"));
 });
 
-
+// morgan middleware
 app.use(morgan(function (tokens, req, res) {
     return  [
         req.method,
@@ -33,8 +62,19 @@ app.use(morgan(function (tokens, req, res) {
         JSON.stringify(req.body)]
   }));
 
+
 // retrieving all the phone book object
-app.use('/', getRouters);
+//app.use('/', getRouters);
+app.get('/api/persons', (req, res) => {
+    Entry.find({})
+    .then(result => {
+        response.json(result);
+        mongoose.connection.close();
+    })
+    .catch(error => {
+        console.log(error);
+    })
+})
 
  // deleting specific entry by id
 app.use('/api/persons/', deleteRouter);
---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://Arnon-Asquira:${password}@cluster0.sihgr.mongodb.net/arnonsfirstdatabase?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+
+const phoneEntrySchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Entry = mongoose.model('Entry', phoneEntrySchema);
+
+if (process.argv.length >= 5) {
+    const entry = new Entry({
+        name: process.argv[3],
+        number: process.argv[4],
+     })
+     
+     entry.save().then(result => {
+       console.log(`added ${entry.name} number ${entry.number} to phone book`)
+       mongoose.connection.close();
+     })
+     .catch(error => console.log(error))
+} else {
+    Entry.find({})
+    .then(result => {
+        console.log(...result);
+        mongoose.connection.close();
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
